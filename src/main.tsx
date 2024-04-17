0296374f788bce04f30269e5a26afdf1f1b4b28b
Erik <erikgraciosa@gmail.com>
2024-04-17 10:31:09 -0700

    setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import App from './App';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/helloworld",
+    element: <div>Hello World</div>,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
